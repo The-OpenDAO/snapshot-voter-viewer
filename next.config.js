77dfa9fb3e8fe3bfa83f9025db073487ba08a626
Tony Ke <thagki9@outlook.com>
2022-03-30 18:05:48 +0800

    Fix asset prefix under CI

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+function getAssetPrefix() {
+  if (process.env.GITHUB_ACTIONS === "true") {
+    const [, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+    return `${repo.toLowerCase()}/`;
+  }
+
+  return "";
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: getAssetPrefix(),
 }
 
 module.exports = nextConfig
