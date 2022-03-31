aef349f899bfa62a1046c979c50a2f498c695add
THaGKI9 <ThaGKI9@outlook.com>
2022-03-31 06:01:45 +0000

    Deploying to gh-pages from @ The-OpenDAO/snapshot-voter-viewer@b8b86109ec4bd0658b8963afc74f616f2fe34b33 🚀

--- _next/static/V14IahIqyC-CcGdE5zZjt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-c2ffd8d428a8879e.js","static/chunks/58-51ac68a832ee2cd7.js","static/chunks/pages/index-b42ba56a725f7af4.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
